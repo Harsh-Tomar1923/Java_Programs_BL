bead52298030e841296364e4d3f9a758e2bb4dab
Harsh Tomar <harsh.thakur191003@gmail.com>
2025-01-18 15:16:32 +0530

    First commit

---
+++ PracticeProblem.java
@@ -0,0 +1,18 @@
+public class helloWorld{
+	public static void main(String[] args){
+		System.out.println("Even Numbers");
+		for(int i=0;i<=10;i++){
+			if(i%2==0){
+				System.out.print(i+" ");
+			}
+		}
+		System.out.println();
+		System.out.println("Odd Numbers");
+		for(int i=0;i<=10;i++){
+                        if(i%2!=0){
+                                System.out.print(i+" ");
+                        }
+                }
+
+	}
+}
